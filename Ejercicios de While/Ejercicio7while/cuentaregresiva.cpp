320868f7aa8487dc3cf12b61f2c8c50a65b7d20a
Diego22182026 <00242126@uca.edu.sv>
2026-05-13 06:33:45 -0600

    feat: add Ejercicio7While folder and files

---
+++ Ejercicios de While/Ejercicio7while/cuentaregresiva.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int N;
+    cout << "Ingresa un numero: " << endl;
+    cin >> N;
+
+    while (N>0)
+    {
+        N--;
+        cout << N << endl;
+    }
+    
+
+    return 0;
+}
